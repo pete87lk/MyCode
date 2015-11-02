bcec28b0f3bfec925fa4d5f54409ac87e1ed1e07
Peter <pete87lk@gmail.com>
2015-11-02 15:53:15 -0500

    lessons from chapter 3

---
+++ C++/C++ Programming In Easy Steps/Lessons/Chapter3/optimize.cpp
@@ -0,0 +1,33 @@
+//Lesson 21 - optimize.cpp
+
+#include <iostream>
+using namespace std;
+
+int computeFactorials(int, int);
+int factorial(int);
+
+int main()
+{
+	computeFactorials(1, 8);
+	return 0;
+}
+
+int computeFactorials(int num, int max)
+{
+	cout<< "Factorial of " << num << ": ";
+	cout<< factorial(num) <<endl;
+	num++;
+	if(num > max)
+	{
+		return 0;
+	}
+	else
+	{
+		computeFactorials(num, max);
+	}
+}
+
+inline int factorial(int n)
+{
+	return(n == 1)? 1 : (factorial(n - 1)* n);
+}
